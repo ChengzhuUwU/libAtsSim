38a511cab7c18c88369bdbdf71dfc90becf3f16a
huohuo <34520192201429@stu.xmu.edu.cn>
2025-04-29 16:49:59 +0800

    stable in loop=4, not stable in loop=5

--- examples/test_1_waiting_events.cpp
+++ examples/test_1_waiting_events.cpp
@@ -43,7 +43,7 @@ int main()
     Buffer<uint> test_buffer;
     test_buffer.resize(1); test_buffer.set_data(0);
 
-    const uint loop_time = 10;
+    const uint loop_time = 4;
 
     //
     // Test async waiting: Add single value for loop_time times, Odd on the CPU, Even on the GPU
@@ -66,20 +66,21 @@ int main()
             const uint curr_event = 2 * cmd_idx + 1;
             const uint prev_cpu_event = curr_event - 1;
 
-            fast_print(cmd_idx, "GPU Signal", cmd_idx);
+            // fast_print(cmd_idx, "GPU Signal", cmd_idx);
             list_cmd_buffer[cmd_idx] = get_command_list().start_new_list_with_new_buffer();
 
             // Wait for CPU's signal
             {
-                fast_print(cmd_idx, "GPU Waits for CPU", prev_cpu_event);
+                // fast_print(cmd_idx, "GPU Waits for CPU", prev_cpu_event);
                 get_command_list().wait_cpu(get_shared_event(), prev_cpu_event);     
             }
             
             // Launch
             {
+                // fast_format("GPU desire for {}", curr_event);
                 get_command_list().add_task(fn_test_add_gpu);
                 fn_test_add_gpu.bind_ptr(test_buffer);
-                fn_test_add_gpu.bind_constant(prev_cpu_event);
+                fn_test_add_gpu.bind_constant(curr_event);
                 fn_test_add_gpu.launch_async(1);
             }   
             get_command_list().make_fence_with_previous_cmd_buffer(); // If False, The Function May Be Empty
@@ -103,17 +104,18 @@ int main()
             {
                 if (curr_event != 0)
                 {
-                    fast_print(cmd_idx, "CPU Waits for GPU", prev_gpu_idx);
+                    // fast_print(cmd_idx, "CPU Waits for GPU", prev_gpu_idx);
                     list_cmd_buffer[prev_gpu_idx]->waitUntilCompleted();
                 }
             }
             
             // Launch
+            // fast_format("CPU desire for {}", curr_event);
             fn_test_add_cpu(test_buffer, curr_event);
 
             // Signal
             {
-                fast_print(cmd_idx, "CPU Signal", curr_event);
+                // fast_print(cmd_idx, "CPU Signal", curr_event);
                 get_shared_event().event->setSignaledValue(curr_event);
             }
         }
